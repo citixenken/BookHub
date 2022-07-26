67bdb68f35d28e01ab98f01147e47d404801819d
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-07-26 07:27:36 +0300

    setting up Book components; minor styling edits

---
+++ src/components/BooksContainer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function BooksContainer() {
+  return <div>BooksContainer</div>;
+}
+
+export default BooksContainer;
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,8 +1,28 @@
+import React, { useState } from "react";
+import BooksContainer from "../BooksContainer";
+import BookList from "../BookList";
 function Home() {
+  const [loading, setLoading] = useState("");
+
   return (
-    <div id="home">
-      <h1 style={{ color: "firebrick" }}>Welcome To BookHub</h1>
-      <p>A repository for your favourite books...</p>
+    <div className="ui container">
+      <h1 className="ui header huge" style={{ color: "firebrick" }}>
+        Welcome To BookHub
+      </h1>
+      <p>A repository for your favourite books</p>
+
+      {/* SEARCH INPUT */}
+      <div className="ui fluid action input">
+        <input type="text" placeholder="Search Books" />
+        <select className="ui compact selection dropdown">
+          <option value="name">Name</option>
+          <option selected="" value="isbn">
+            ISBN
+          </option>
+        </select>
+        <div className="ui button">Search</div>
+        <BooksContainer />
+      </div>
     </div>
   );
 }
