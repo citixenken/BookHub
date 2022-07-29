d825f6b3041a1df8045ab07e85d36749b3823df8
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-07-30 02:05:39 +0300

    api edit

---
+++ src/api.js
@@ -0,0 +1,19 @@
+export const FETCH_OPTIONS = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "97c67bebe1mshd809c1f20b185d1p194924jsnd8f02d67c42b",
+    "X-RapidAPI-Host": "hapi-books.p.rapidapi.com",
+  },
+};
+
+export const HAPI_BOOKS_API_URL = "https://hapi-books.p.rapidapi.com";
+export const GET_BOOK_BY_NAME =
+  "https://hapi-books.p.rapidapi.com/search/the+walking+dead";
+
+export const GET_BOOK_BY_ID = "https://hapi-books.p.rapidapi.com/book/56597885";
+export const GET_BOOK_BY_POPULAR_WEEKLY =
+  "https://hapi-books.p.rapidapi.com/week/horror";
+export const GET_BOOK_BY_POPULAR_MONTHLY =
+  "https://hapi-books.p.rapidapi.com/month/2022/3";
+export const GET_BOOK_BY_GENRE_ANNUAL =
+  "https://hapi-books.p.rapidapi.com/nominees/fiction/2020";
