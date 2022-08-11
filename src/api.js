a4a9bb59cafbb0dd2b9c17ff942f59a810d09ea0
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-08-11 11:55:44 +0300

    minor edits

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 export const FETCH_OPTIONS = {
   method: "GET",
   headers: {
-    // "X-RapidAPI-Key": `${process.env.REACT_APP_HAPI_KEY}`,
-    "X-RapidAPI-Key": "26bb38a6cdmshd49a4e8f862b4b4p1aaa88jsnb2b1c078217a",
+    "X-RapidAPI-Key": `${process.env.REACT_APP_HAPI_KEY}`,
+    // "X-RapidAPI-Key": "26bb38a6cdmshd49a4e8f862b4b4p1aaa88jsnb2b1c078217a",
     "X-RapidAPI-Host": "hapi-books.p.rapidapi.com",
   },
 };
